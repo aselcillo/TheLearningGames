61bca383bc959b0b50e4ed2ff997934630885f1c
Asel Acebedo Romalde <aselromalde@gmail.com>
2017-06-06 20:52:23 +0200

    Fixed a translation reference

--- myapp/www/js/controllers/signUpController.js
+++ myapp/www/js/controllers/signUpController.js
@@ -28,25 +28,25 @@ function ($scope, $stateParams, $http, $state, sharedData, $ionicLoading, $trans
   /**
     Needed for the translations to work in the controller's words.
   */
-  $translate(['CHECK_EMAIL_TO_VERIFY', 'EMAIL_INVALID', 'ERROR_ACCESS_UNKNOW', 'EMAIL_ALREADY_USED', 'ERROR_WEAK_PASSWORD', 'SCHOOL_NOT_ESTABLISHED']).then(function(translations) {
-    $scope.checkEmailToVerify = translations.CHECK_EMAIL_TO_VERIFY;
+  $translate(['EMAIL_INVALID', 'ERROR_ACCESS_UNKNOW', 'EMAIL_ALREADY_USED', 'ERROR_WEAK_PASSWORD', 'SCHOOL_NOT_ESTABLISHED', 'VERIFY_EMAIL']).then(function(translations) {
     $scope.emailInvalidAlert = translations.EMAIL_INVALID;
     $scope.errorEmailUsedAlert = translations.EMAIL_ALREADY_USED;
     $scope.errorUnknowAlert = translations.ERROR_ACCESS_UNKNOW;
     $scope.schholNotEstablished = translations.SCHOOL_NOT_ESTABLISHED;
     $scope.weakPasswordAlert = translations.ERROR_WEAK_PASSWORD;
+    $scope.checkEmailToVerify = translations.VERIFY_EMAIL;
   });
 
   /**
     Needed for the translations to change their value in execution time.
   */
   $rootScope.$on('$translateChangeSuccess', function () {
-    $scope.checkEmailToVerify = $translate.instant('CHECK_EMAIL_TO_VERIFY');
     $scope.emailInvalidAlert = $translate.instant('EMAIL_INVALID');
     $scope.errorEmailUsedAlert = $translate.instant('EMAIL_ALREADY_USED');
     $scope.errorUnknowAlert = $translate.instant('ERROR_ACCESS_UNKNOW');
     $scope.schholNotEstablished = $translate.instant('SCHOOL_NOT_ESTABLISHED');
     $scope.weakPasswordAlert = $translate.instant('ERROR_WEAK_PASSWORD');
+    $scope.checkEmailToVerify = $translate.instant('VERIFY_EMAIL');
   });
 
   $scope.modelSignUp = {};
